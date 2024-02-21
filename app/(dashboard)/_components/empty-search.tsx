39c2b43412798fab48ca9fb4483fc4780d90bc85
Jethro Au <galongau@gmail.com>
2024-02-21 00:13:11 +0000

    fix typo

--- app/(dashboard)/_components/empty-search.tsx
+++ app/(dashboard)/_components/empty-search.tsx
@@ -7,7 +7,7 @@ export const EmptySearch = (props: Props) => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <Image src={"/empty-search.svg"} alt={"Empty"} height={140} width={140} />
-      <h2 className="mt-6 text-2xl font-semibold">No results!</h2>
+      <h2 className="mt-6 text-2xl font-semibold">No result!</h2>
       <p className="text-muted-foreground">Try searching for something else</p>
     </div>
   );
